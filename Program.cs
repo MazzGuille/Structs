91f81196f0e4e27960dc4e84148ae71e6a05cf5e
MazzGuille <mazzcode@gmail.com>
2022-05-25 01:24:38 -0300

    Struct 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿WriteLine("Hello, World!");
+Empleado empleado1 = new(1200, 500);
+empleado1.CambiaSalario(empleado1, 500);
+empleado1.CambiaComision(empleado1, 50);
+WriteLine(empleado1);
+WriteLine($"Total a pagar al empleado: {empleado1.salarioBase + empleado1.comision}");
+
+
---
+++ Structs/Empleado.cs
@@ -0,0 +1,31 @@
+﻿namespace Empleados
+{
+    struct Empleado
+    {
+        public double salarioBase, comision;
+
+        public Empleado(int salarioBase, int comision)
+        {
+            this.salarioBase = salarioBase;
+
+            this.comision = comision;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Salario y comision del empleado {0}, {1}", this.salarioBase, this.comision);
+        }
+
+        public void CambiaSalario(Empleado emp, double incremento)
+        {
+            emp.salarioBase += incremento;
+
+        }
+
+        public void CambiaComision(Empleado emp, double incremento)
+        {
+            emp.comision += incremento;
+
+        }
+    }
+}
